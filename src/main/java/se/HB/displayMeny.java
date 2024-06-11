c7ebc7b78063a2460d7413141b6940d1d242ad28
Hbrodd <habr1901@gmail.com>
2024-06-11 12:15:55 +0200

    Small changes

--- src/main/java/se/HB/displayMeny.java
+++ src/main/java/se/HB/displayMeny.java
@@ -6,7 +6,7 @@ public class displayMeny {
     public static void convertDisplay () {
         System.out.println("Choose a number from the menu to choose what currency you want to convert");
         System.out.println("1: SEK to USD");
-        System.out.println("2: USD to SEk");
+        System.out.println("2: USD to SEK");
         System.out.println("3: SEK to EUR");
         System.out.println("4: EUR to SEK");
         System.out.println("5: Exit");
